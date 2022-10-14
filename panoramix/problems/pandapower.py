496dad07074ac2449ea583c8fd9212067be19e85
bdonon <balthazar.donon@gmail.com>
2022-10-14 15:58:19 +0200

    New voltage control metrics.

--- panoramix/problems/pandapower.py
+++ panoramix/problems/pandapower.py
@@ -11,7 +11,7 @@ class PandaPowerVoltageControl(AbstractProblem):
     backend = PandaPowerBackend()
     data_structure = {
         'bus': {"feature_names": ['res_vm_pu', 'max_vm_pu', 'min_vm_pu']},
-        'gen': {"feature_names": ['res_q_mvar', 'vm_pu']},
+        'gen': {"feature_names": ['res_q_mvar', 'vm_pu', 'min_q_mvar', 'max_q_mvar']},
         'load': {"feature_names": ['p_mw']},
         'line': {"feature_names": ['res_pl_mw', 'res_loading_percent']},
         'trafo': {"feature_names": ['res_pl_mw', 'res_loading_percent']}
@@ -27,6 +27,9 @@ class PandaPowerVoltageControl(AbstractProblem):
             "Total Joule Losses (MW)": total_joule_losses,
             "Normalized Joule Losses": normalized_joule_losses,
             "Bus Voltage (p.u.)": bus_voltage,
+            "Normalized Bus Voltage": normalized_bus_voltage,
+            "Normalized Branch Current": normalized_branch_current,
+            "Normalized Reactive Generation": normalized_generation_reactive_power,
             "Bus Over Voltage": over_voltage,
             "Bus Under Voltage": under_voltage,
             "Bus Illicit Voltage": illicit_voltage,
@@ -55,6 +58,12 @@ def normalized_joule_losses(values):
 def bus_voltage(values):
     return values['bus']['features']['res_vm_pu']
 
+def normalized_bus_voltage(values):
+    v = values['bus']['features']['res_vm_pu']
+    v_min = values['bus']['features']['min_vm_pu']
+    v_max = values['bus']['features']['max_vm_pu']
+    return (v-v_min) / (v_max - v_min)
+
 def over_voltage(values):
     return values['bus']['features']['res_vm_pu'] > values['bus']['features']['max_vm_pu']
 
@@ -70,6 +79,11 @@ def line_loading_percent(values):
 def trafo_loading_percent(values):
     return values['trafo']['features']['res_loading_percent']
 
+def normalized_branch_current(values):
+    line_normalized_branch_current = values['line']['features']['res_loading_percent'] / 100.
+    trafo_normalized_branch_current = values['trafo']['features']['res_loading_percent'] / 100.
+    return np.concatenate([line_normalized_branch_current, trafo_normalized_branch_current])
+
 def over_current(values):
     line_over_current = line_loading_percent(values) > 100
     trafo_over_current = trafo_loading_percent(values) > 100
@@ -77,6 +91,12 @@ def over_current(values):
 
 def generation_reactive_power(values):
     return values['gen']['features']['res_q_mvar']
+
+def normalized_generation_reactive_power(values):
+    q = values['gen']['features']['res_q_mvar']
+    q_min = values['gen']['features']['min_q_mvar']
+    q_max = values['gen']['features']['max_q_mvar']
+    return (q - q_min) / (q_max - q_min)
 
 def generation_voltage_setpoint(values):
     return values['gen']['features']['vm_pu']

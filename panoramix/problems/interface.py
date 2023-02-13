8c169b980ce823379c0a0e24642c389f366bfe81
bdonon <balthazar.donon@gmail.com>
2023-02-13 15:13:56 +0100

    Solve power flow with initialization at previous results.

--- panoramix/problems/interface.py
+++ panoramix/problems/interface.py
@@ -44,7 +44,7 @@ class AbstractProblem(ABC):
     def add_assessment_row_old(self, file, table_dict):
         """Imports and simulates a file, computes metrics and appends them to table_dict."""
         net = self.backend.load_network(file)
-        self.backend.run_network(net, enforce_q_lims=True, delta_q=0.)
+        self.backend.run_network(net, init='results', enforce_q_lims=True, delta_q=0.)
         values = self.backend.get_data_network(net, self.data_structure)
         for key, metrics in self.metrics_dict.items():
             table_dict[key].append(metrics(values))
@@ -53,7 +53,7 @@ class AbstractProblem(ABC):
         """Imports and simulates a file, computes metrics and appends them to table_dict."""
         net = self.backend.load_network(file)
         sample_name = os.path.splitext(os.path.basename(file))[0]
-        self.backend.run_network(net, enforce_q_lims=True, delta_q=0.)
+        self.backend.run_network(net,  init='results', enforce_q_lims=True, delta_q=0.)
         values = self.backend.get_data_network(net,
                                                feature_names=self.feature_names,
                                                address_names=self.address_names,
